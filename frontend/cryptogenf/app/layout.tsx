01cc550507a55e2594b7000edc3f683993f89c0e
Caique <asapcaique7@gmail.com>
2025-01-24 12:37:00 -0300

    frontend: wagmi provider config

--- frontend/cryptogenf/app/layout.tsx
+++ frontend/cryptogenf/app/layout.tsx
@@ -1,21 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import SessionProvider  from './providers/authProviders'
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import WagmiProviderComp from "@/lib/wagmi/wagmi-provider";
+import { headers } from "next/headers";
+import { cookieToInitialState } from "wagmi";
+import { config } from "@/lib/wagmi/config";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "CryptoGen",
@@ -28,13 +20,14 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const session = await getServerSession(authOptions);
+  const initialState = cookieToInitialState(config, headers().get("cookie"));
   return (
     <SessionProvider session={session}>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <WagmiProviderComp initialState={initialState}>
         {children}
+        </WagmiProviderComp>
       </body>
     </html>
     </SessionProvider>
